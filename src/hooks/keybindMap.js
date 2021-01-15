8a4ceb797772b7ece67190ac910c85c21f81ad7c
Zachary Cranfill <zcranfill@zcranfill-mac.lan>
2021-01-14 20:38:51 -0500

    changing keybinds to general behaviors

--- src/hooks/keybindMap.js
+++ src/hooks/keybindMap.js
@@ -1,7 +1,8 @@
 const Keybinds = {
     Keybinds: [
-        { key: 'g', code: '71', color: { r: '241', g: '112', b: '19', a: '1' }, behavior: "Grooming", active: false },
-        { key: 's', code: '83', color: { r: '41', g: '112', b: '200', a: '1' }, behavior: "Scratching", active: false }
+        { key: '1', code: '49', color: { r: '241', g: '112', b: '19', a: '1' }, behavior: "Behavior1", active: false },
+        { key: '2', code: '50', color: { r: '41', g: '112', b: '200', a: '1' }, behavior: "Behavior2", active: false },
+        { key: '3', code: '51', color: { r: '41', g: '112', b: '200', a: '1' }, behavior: "Behavior3", active: false }
     ]
 };
 
